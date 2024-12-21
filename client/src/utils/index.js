e73315e872c18142caf720c493cd899b5e45f987
prakash9724 <hellobrother91069@gmail.com>
2024-12-21 15:44:26 +0530

    download error

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,18 +1,15 @@
 import FileSaver from 'file-saver';
+import { surpriseMePrompts } from '../constant';
 
-export const surpriseMePrompts = [
-  // ... your prompts
-];
-
-export const getRandomPrompts = (prompt) => {
+export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
   const randomPrompt = surpriseMePrompts[randomIndex];
 
-  if (randomPrompt === prompt) return getRandomPrompts(prompt);
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
   return randomPrompt;
-};
+}
 
-export const downloadImage = async (_id, photo) => {
+export async function downloadImage(_id, photo) {
   FileSaver.saveAs(photo, `download-${_id}.jpg`);
-};
+}
