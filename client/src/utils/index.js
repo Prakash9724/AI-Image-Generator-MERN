9681748d2ecd150b308a0d4e1d05b0bcdeac331b
prakash9724 <hellobrother91069@gmail.com>
2024-12-21 15:58:58 +0530

    download error fixing

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,11 +1,11 @@
 import FileSaver from 'file-saver';
 import { surpriseMePrompts } from '../constants';
 
-export function getRandomPrompts(prompt) {
+export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
   const randomPrompt = surpriseMePrompts[randomIndex];
 
-  if (randomPrompt === prompt) return getRandomPrompts(prompt);
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
   return randomPrompt;
 }
